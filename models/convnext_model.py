3aa259a3365e37ff3205090efc0aac5459974e3b
Gayathri10061999 <92244699+Gayathri10061999@users.noreply.github.com>
2026-08-05 17:14:20 +0530

    Create convnext_model.py

---
+++ models/convnext_model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from torchvision.models import convnext_tiny, ConvNeXt_Tiny_Weights
+
+def build_model(num_classes):
+    model = convnext_tiny(weights=ConvNeXt_Tiny_Weights.DEFAULT)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    in_features = model.classifier[2].in_features
+
+    model.classifier[2] = nn.Linear(in_features, num_classes)
+
+    return model
